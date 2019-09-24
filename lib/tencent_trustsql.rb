c01d45789343bd2ca7278a1ce6f6be8affa8f94c
hzy <18674699548@163.com>
2019-09-24 21:59:51 +0800

    fix

--- lib/tencent_trustsql.rb
+++ lib/tencent_trustsql.rb
@@ -1,12 +1,18 @@
 require "tencent_trustsql/version"
+require 'ecdsa'
+require 'securerandom'
 
 module TencentTrustsql
-  class Error < StandardError; end
-
 
   # 产生一对公私钥, 并返回
   def self.generatePairKey
     group = ECDSA::Group::Secp256k1
     p private_key = 1 + SecureRandom.random_number(group.order - 1)
   end
+
+end
+
+
+module TencentTrustsql
+  class Error < StandardError; end
 end
