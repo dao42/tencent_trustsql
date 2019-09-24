4d81206e088e38ada0ad0b08627ee1f7811aebb1
jack <qinshenyong@socialdatamax.com>
2019-09-24 18:02:45 +0800

    init gem

---
+++ lib/tencent_trustsql.rb
@@ -0,0 +1,6 @@
+require "tencent_trustsql/version"
+
+module TencentTrustsql
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/tencent_trustsql/version.rb
@@ -0,0 +1,3 @@
+module TencentTrustsql
+  VERSION = "0.1.0"
+end
