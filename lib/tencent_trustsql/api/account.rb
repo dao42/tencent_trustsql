e1e0f327f104c844ba6733230db19cefbe08be76
jack <qinshenyong@socialdatamax.com>
2019-09-27 18:40:38 +0800

    fix

--- lib/tencent_trustsql/api/account.rb
+++ lib/tencent_trustsql/api/account.rb
@@ -2,42 +2,13 @@ module TencentTrustsql
   module Api
     module Account
 
-      def params_sort(params={})
-        params.sort.map do |k, v|
-          "#{k}=#{v}" if v.to_s != ''
-        end.compact.join('&')
-      end
-
-      def apply(options={})
+      def apply_account(private_key_out,options={})
         url = "https://baas.qq.com/tpki/tpki.TpkiSrv.AccountApply"
-
         public_key_out = TencentTrustsql.encoded_public_key private_key_out
-
-        params = {
-            version: '1.0',
-            sign_type: 'ECDSA',
-            mch_id: mch_id,
-            user_pub_key: public_key_out,
-            timestamp: Time.now.to_i
-        }
+        p u_base_params
+        params = u_base_params.merge!({ acc_pub_key: public_key_out })
         params.merge!(options)
-
-        p params_sort(params)
-
-        p sign = TencentTrustsql.sign(mch_private_key, query)
-        p sign_out = TencentTrustsql.output_formatter.out_sign(sign)
-
-        params.merge!({mch_sign: sign_out})
-
-        response =HTTP.post(url, :form => params)
-        JSON.parse(response.body)
-
-      end
-
-      def get(options={})
-        url = "https://baas.qq.com/tpki/tpki.TpkiSrv.UserGet"
-        public_key_out = TencentTrustsql.encoded_public_key private_key_out
-
+        send_request(url,params)
       end
 
     end
