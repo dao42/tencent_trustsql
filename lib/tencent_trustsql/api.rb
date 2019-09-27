a60896d97bd556ad9bf2c7f2097c19a6281d8fa9
hzy <18674699548@163.com>
2019-09-27 15:30:02 +0800

    fix

--- lib/tencent_trustsql/api.rb
+++ lib/tencent_trustsql/api.rb
@@ -1,3 +1,23 @@
 Dir["#{File.dirname(__FILE__)}/api/*.rb"].each do |path|
   require path
 end
+module TencentTrustsql
+  module API
+
+    module AssetIssue
+      BASE_PARAMS = {}
+    end
+
+    module Iss
+      BASE_PARAMS = {}
+    end
+
+    module CgiBin
+      BASE_PARAMS = {}
+    end
+
+    module Tpki
+    end
+
+  end
+end
